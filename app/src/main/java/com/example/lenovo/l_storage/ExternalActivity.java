88741e0a5899870ba0458a4355f065b1a7a6b42c
jccjd <1340944538@qq.com>
2018-01-01 00:12:37 +0800

    qq

--- app/src/main/java/com/example/lenovo/l_storage/ExternalActivity.java
+++ app/src/main/java/com/example/lenovo/l_storage/ExternalActivity.java
@@ -1,18 +1,20 @@
 package com.example.lenovo.l_storage;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.*;
 
 public class ExternalActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -58,15 +60,57 @@ public class ExternalActivity extends AppCompatActivity implements View.OnClickL
             } else {
             doSaveData();
         }
-
-
     }
 
     private void doSaveData() {
+        String strData = etInputData.getText().toString();
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File file = new File(Environment.getExternalStorageDirectory(),"data.txt");
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(strData.getBytes());
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     private void getData() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ) {
+            File file = new File(Environment.getExternalStorageDirectory(),"data.txt" );
+            try {
+                FileInputStream fis = new FileInputStream(file);
+                StringBuffer sb = new StringBuffer();
+                byte[] buffer = new byte[512];
+                int hasread = -1;
+                while ((hasread = fis.read(buffer))!= -1) {
+                    sb.append(new String(buffer,0,hasread));
 
+                }
+                tvShowData.setText(sb.toString());
+                fis.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1 :
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doSaveData();
+                }
+                break;
+        }
     }
 }
